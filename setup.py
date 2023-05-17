48ad9abd6e974f9f8dfad29b21ed0f2960fae1d6
tzuhsuanchangX <tzuhsuanchang@proton.me>
2023-05-17 17:48:41 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rdquantum",
-    version="2023.04.22",
-    install_requires=["gymnasium==0.28.1", "qutip==4.7.0"],
+    version="2023.05.17",
+    install_requires=["gymnasium==0.26.3", "ray[rllib]", "qutip==4.7.0", "tensorflow", "torch"],
     packages=find_packages(),
 )
