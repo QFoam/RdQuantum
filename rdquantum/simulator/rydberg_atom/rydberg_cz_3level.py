2d7d86125e698f36f0443e6b55dce8b177e51912
tzuhsuanchangX <tzuhsuanchang@proton.me>
2023-05-25 17:12:58 +0800

    Modified Rydberg_Cz_3Level

--- rdquantum/simulator/rydberg_atom/rydberg_cz_3level.py
+++ rdquantum/simulator/rydberg_atom/rydberg_cz_3level.py
@@ -1,2 +1,21 @@
-class Rydberg_Cz_3Level():
+import qutip  as qt
 
+class Rydberg_Cz_3Level():
+    def __init__(
+            self
+            ):
+        """ RL parameters
+        """
+        self.action_space = spaces.Dict(
+            {
+                'omega_p_amp': spaces.Box(-1, 1, shape=(), dtype=np.float32)
+                'omega_r_amp': spaces.Box(-1, 1, shape=(), dtype=np.float32),
+                'delta_p_amp': spaces.Box(-1, 1, shape=(), dtype=np.float32),
+                'gate_time': spaces.Box(-1, 1, shape=(), dtype=np.float32)
+            }
+        )
+
+    def gate(
+            self,
+            ):
+        return results
